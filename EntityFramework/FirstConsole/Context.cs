ad3eeda0ed917294924fcbf769c90add888ae23a
amvincible <ljlovemichael@hotmail.com>
2016-08-25 03:02:50 +0800

    using EF create a database and seed the database

---
+++ EntityFramework/FirstConsole/Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsole
+{
+    public class Context:DbContext
+    {
+        public Context()
+           : base("name=FirstConsole")
+        {
+        }
+
+        public DbSet<Donator> Donators { get; set; }
+    }
+}
---
+++ EntityFramework/FirstConsole/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Context())
+            {
+                try
+                {
+                    bool created = context.Database.CreateIfNotExists();
+                    if (created)
+                    {
+
+                        Console.WriteLine("Database is created.");
+                       
+                    }
+                    else
+                        Console.WriteLine("Database was NOT created.");
+                    var donators = new List<Donator>
+                             {
+                            new Donator
+                            {
+                              Name   = "陈志康",
+                              Amount = 50,
+                              DonateDate = new DateTime(2016, 4, 7)
+                            },
+                            new Donator
+                            {
+                                Name = "海风",
+                                Amount = 5,
+                                DonateDate = new DateTime(2016, 4, 8)
+                            },
+                            new Donator
+                            {
+                                Name = "醉千秋",
+                                Amount = 18.8m,
+                                DonateDate = new DateTime(2016, 4, 15)
+                            }
+                        };
+
+                    context.Donators.AddRange(donators);
+                    context.SaveChanges();
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
